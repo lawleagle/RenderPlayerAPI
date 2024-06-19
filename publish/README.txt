606ea02f1529642d23a7f8c8330153af92233298
Manu <lawleagle@gmail.com>
2024-06-19 09:14:06 +0100

    migrated to GTNH build system

--- publish/README.txt
+++
@@ -1,2 +0,0 @@
-# How to publish
-sh ./build_and_release.sh $GITHUB_TOKEN [$CURSEFORGE_TOKEN] [$MODRINTH_TOKEN]
--- src/main/java/api/player/forge/RenderPlayerAPIContainer.java
+++ src/main/java/api/player/forge/RenderPlayerAPIContainer.java
@@ -1,16 +1,16 @@
 // ==================================================================
 // This file is part of Render Player API.
-// 
+//
 // Render Player API is free software: you can redistribute it and/or
 // modify it under the terms of the GNU Lesser General Public License
 // as published by the Free Software Foundation, either version 3 of
 // the License, or (at your option) any later version.
-// 
+//
 // Render Player API is distributed in the hope that it will be
 // useful, but WITHOUT ANY WARRANTY; without even the implied
 // warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 // See the GNU Lesser General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU Lesser General Public
 // License and the GNU General Public License along with Render
 // Player API. If not, see <http://www.gnu.org/licenses/>.
@@ -20,6 +20,7 @@ package api.player.forge;
 
 import java.util.*;
 import com.google.common.eventbus.*;
+import api.player.Tags;
 import cpw.mods.fml.common.*;
 
 public class RenderPlayerAPIContainer extends DummyModContainer
@@ -40,7 +41,7 @@ public class RenderPlayerAPIContainer extends DummyModContainer
 
 		meta.modId = "RenderPlayerAPI";
 		meta.name = "Render Player API";
-		meta.version = RenderPlayerAPIPlugin.Version;
+		meta.version = Tags.VERSION;
 		meta.description = "Render Player API for Minecraft Forge";
 		meta.url = "http://www.minecraftforum.net/topic/1261354-";
 		meta.authorList = Arrays.asList(new String[] { "Divisor" });
